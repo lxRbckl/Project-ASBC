6de0ac468604897940022ce237abf770736e7a98
lxRbckl <lxrbckl@gmail.com>
2023-05-06 19:16:29 -0500

    introduction of project structure

---
+++ main.py
@@ -0,0 +1,25 @@
+# Project Ubenyo by Alex Arbuckle #
+
+
+# import <
+from os import system
+
+# >
+
+
+# global <
+applicationPath = '/Application/BlueStacks.app'
+
+
+# >
+
+
+
+
+
+# main <
+if (__name__ == '__main__'):
+
+    pass
+
+# >
