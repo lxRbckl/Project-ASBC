77a1010082a40a9acbe0961d3475b703e94d843f
lxRbckl <lxrbckl@gmail.com>
2023-05-27 15:45:59 -0500

    revision of bash script for boot runtime variation

--- script.sh
+++ script.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
 
-nohup poetry run python3 -c 'from frontend.frontend import run; run()' &
-nohup poetry run python3 -c 'from backend.bot import run; run()' &
+# locate where executable is (which poetry) #
+POETRY='/Library/Frameworks/Python.framework/Versions/3.8/bin/poetry'
+
+
+# using path to executable execute the following #
+$POETRY run python3 -c 'from backend.bot import run; run()'
+$POETRY run python3 -c 'from frontend.frontend import run; run()'
