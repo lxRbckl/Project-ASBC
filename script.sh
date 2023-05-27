74ff55b9224d9821402db7c2b91cd6a9c49563d1
Alex Arbuckle <84994842+lxRbckl@users.noreply.github.com>
2023-05-27 16:46:13 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
 
+sleep 200
+
+
 # locate where executable is (which poetry) #
 POETRY='/Library/Frameworks/Python.framework/Versions/3.8/bin/poetry'
 
 
 # using path to executable execute the following #
-# $POETRY run python3 -c 'from backend.bot import run; run()'
-$POETRY run python3 -c 'from frontend.frontend import run; run()'
+$POETRY run python3 -c 'from backend.bot import run; run()' &
+$POETRY run python3 -c 'from frontend.frontend import run; run()' &
