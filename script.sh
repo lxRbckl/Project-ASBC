8d9de6b9a4fbd6927f2c0ae92dd0fa11ae932d28
lxRbckl <lxrbckl@gmail.com>
2023-05-28 20:12:50 -0500

    c

--- script.sh
+++ script.sh
@@ -1,10 +1,5 @@
 #! /bin/bash
 
 
-# locate where executable is (which poetry) #
-POETRY='/Library/Frameworks/Python.framework/Versions/3.8/bin/poetry'
-
-
-# using path to executable execute the following #
-$POETRY run python3 -c 'from frontend.layout import run; run()' &
-$POETRY run python3 -c 'from backend.bot import run; run()'
+poetry run python3 -c 'from frontend.layout import run; run()' &
+poetry run python3 -c 'from backend.bot import run; run()'
